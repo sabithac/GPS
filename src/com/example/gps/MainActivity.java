3c5d9f181bedb138ab956235c21c2dd41b3196c3
sabithac <sabithathamizh@gmail.com>
2017-03-13 14:11:28 +0530

    Third Commit

--- src/com/example/gps/MainActivity.java
+++ src/com/example/gps/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.gps;
 
 import android.support.v7.app.ActionBarActivity;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+ @SuppressLint("NewApi") public class MainActivity extends ActionBarActivity implements LocationListener{
 
-public class MainActivity extends ActionBarActivity {
-
+	private LocationManager locationmanager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationmanager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationmanager.requestLocationUpdates( LocationManager.GPS_PROVIDER,1000,10, this);
     }
 
     @Override
@@ -32,4 +39,28 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+		
+	}
 }
